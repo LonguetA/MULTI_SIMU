c2a28bae2e42a63c66ce613be1df8193f9245ccf
XIV <65488503+LonguetA@users.noreply.github.com>
2023-02-09 10:44:31 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+# DIR FILE 
+PWD=
+source /users/outil/soc/env_soclib.sh
+
+# CHANGE tp1.desc 
+ssh berlioz "cd $PWD && echo $PWD && source /users/outil/soc/env_soclib.sh && /users/outil/soc/soclib/utils/bin/soclib-cc -p tp1.desc -t systemc_21 -o simul.x && exit "
+
+source /users/outil/soc/env_soclib.sh
